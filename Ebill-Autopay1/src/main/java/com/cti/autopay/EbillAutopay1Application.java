38c855a032c48962cacb155d478c841e51994d1d
kiran <kiranpatil123>
2020-12-14 15:37:35 +0530

    initial commit

---
+++ Ebill-Autopay1/src/main/java/com/cti/autopay/EbillAutopay1Application.java
@@ -0,0 +1,13 @@
+package com.cti.autopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbillAutopay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbillAutopay1Application.class, args);
+	}
+
+}
---
+++ Ebill-Autopay1/src/test/java/com/cti/autopay/EbillAutopay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cti.autopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbillAutopay1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
